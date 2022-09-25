a439b87f2b623391172a127222da882dc3773d4e
renearide <96728526+renearide@users.noreply.github.com>
2022-09-25 11:29:23 -0300

    Update admin.py

--- emptimelogmgmt/emptimeclklogmgmt/admin.py
+++ emptimelogmgmt/emptimeclklogmgmt/admin.py
@@ -1,7 +1,5 @@
-import csv
 from django.contrib import admin
 from . import models
-from django.http import HttpResponse
 
 admin.site.register(models.Employee)
 admin.site.register(models.EmployeeInfo)
@@ -9,21 +7,3 @@ admin.site.register(models.Emp)
 admin.site.register(models.user)
 admin.site.register(models.Status)
 admin.site.register(models.Work)
-class ExportCsvMixin:
-    actions = ('export_as_csv',)
-    def export_as_csv(self, request, queryset):
-
-        meta = self.model._meta
-        field_names = [field.name for field in meta.fields]
-
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
-        writer = csv.writer(response)
-
-        writer.writerow(field_names)
-        for obj in queryset:
-            row = writer.writerow([getattr(obj, field) for field in field_names])
-
-        return response
-
-    export_as_csv.short_description = "Export Selected"
